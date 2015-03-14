8136a9ae46674f0d9ea01c95a98eb578d70a5e56
Nicolas Garneau <ngarneau@ngarneau.com>
2015-03-14 15:56:31 -0400

    Modifiy the sandbox python file

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 import numpy as np
 
 # Utilities
@@ -23,24 +21,48 @@ from sklearn.metrics import classification_report
 # Plot
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+get_ipython().magic(u'matplotlib inline')
 
 
 # Load necessary datasets
 X, y = load_svmlight_file("../dataset/dataset")
 X_mystery, y_mystery = load_svmlight_file("../dataset/mystery")
 
+
 #-------
 # Preprocess the data
 # We can perform several preprocessing steps on our dataset.
 # Here is one example of scaling the dataset.
+# See also:
+#    http://scikit-learn.org/stable/modules/preprocessing.html#normalization
 #-------
 print X.toarray()[0][:20]
 
-scaler = preprocessing.StandardScaler(with_mean=False).fit(X)
-X = scaler.transform(X)
-X_mystery = scaler.transform(X_mystery)
+X = preprocessing.StandardScaler().fit_transform(X.toarray())
 
-print X.toarray()[0][:20]
+X_mystery = preprocessing.StandardScaler().fit_transform(X_mystery.toarray())
+
+print X[0][:20]
+
+
+#--------
+# Simple validation
+# using train and test sets
+#--------
+model = neighbors.KNeighborsClassifier(n_neighbors=35)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0)
+model.fit(X_train, y_train)
+print model.score(X_test, y_test)
+
+
+#--------
+# Cross validation
+#--------
+model = neighbors.KNeighborsClassifier(n_neighbors=35, weights='distance')
+scores = cross_validation.cross_val_score(model, X, y, verbose=3, cv=10)
+print scores
+print scores.mean()
+
 
 #--------
 # Grid search
@@ -78,17 +100,32 @@ def cross_val():
         y_true, y_pred = y_test, clf.predict(X_test)
         print(classification_report(y_true, y_pred))
         print()
-
+    
 cross_val()
 
 
-#--------
-# Predict
-# Use the whole dataset to train then predict the label on the mystery dataset
-# Save the predicted values into svmlight format.
-#--------
+# Predict using the best hyper parameter
 model = neighbors.KNeighborsClassifier(n_neighbors=35)
 model.fit(X, y)
 y_pred = model.predict(X_mystery)
-dump_svmlight_file(X_mystery, y_pred, 'knn-35')
+
+# Save the predicted labels
+dump_svmlight_file(X_mystery, y_pred, 'ngarneau-scaled')
+
+
+from sklearn.ensemble import BaggingClassifier
+
+bagging = BaggingClassifier(neighbors.KNeighborsClassifier(n_neighbors=35), n_estimators=100, max_samples=1.0, max_features=1.0)
+scores = cross_validation.cross_val_score(bagging, X, y, verbose=3, cv=10)
+
+print scores
+print scores.mean()
+
+
+bagging.fit(X, y)
+y_pred = bagging.predict(X_mystery)
+
+# Save the predicted labels
+dump_svmlight_file(X_mystery, y_pred, 'bagging-knn-35')
+
 
